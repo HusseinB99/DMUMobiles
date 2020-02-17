1008cfa8c975449c8d280083646d1cc4ec59e2ad
Rosen <P2403374@my365.dmu.ac.uk>
2020-02-17 14:53:08 +0000

    Added testing and class for address management

---
+++ DMU Mobile Testing/tstStock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DMUMobilesClasses1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DMU_Mobile_Testing
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create am instance of the class we want to create
+            clsStock aStock = new clsStock();
+            //test to see that it exists
+            Assert.IsNotNull(aStock);
+        }
+    }
+}
