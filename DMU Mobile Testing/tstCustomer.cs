f113db6753424e90ac732434ac0715903c905af6
adamabdullah123 <60107642+adamabdullah123@users.noreply.github.com>
2020-03-09 18:11:47 +0000

    Added testing and class for customer.

---
+++ DMU Mobile Testing/tstCustomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DMUMobilesClasses1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DMU_Mobile_Testing
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomer AnCustomer = new clsCustomer();
+            Assert.IsNotNull(AnCustomer);
+        }
+
+
+
+    }
+}
