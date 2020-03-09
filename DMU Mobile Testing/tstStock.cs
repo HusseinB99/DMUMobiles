2fad4cb97d0ba62c21f19ed966b902ee22b4a9f1
Rosen <P2403374@my365.dmu.ac.uk>
2020-03-09 18:17:20 +0000

    Added all attributes testing and find methods. created get and set method for all classes. Deleted UnitTest1.cs file.

--- DMU Mobile Testing/tstStock.cs
+++ DMU Mobile Testing/tstStock.cs
@@ -7,13 +7,189 @@ namespace DMU_Mobile_Testing
     [TestClass]
     public class tstStock
     {
+        public object StockID { get; private set; }
+
+
         [TestMethod]
-        public void InstanceOK()
+        public void DateAddedPropertyOK()
         {
-            //create am instance of the class we want to create
+            //create an instance of the class we want to create
             clsStock aStock = new clsStock();
-            //test to see that it exists
-            Assert.IsNotNull(aStock);
+            //create some test data to assign to the property
+            DateTime TestData = DateTime.Now.Date;
+            //assign the data to the property
+            aStock.DateAdded = TestData;
+            //test to see that the two values are the same
+            Assert.AreEqual(aStock.DateAdded, TestData);
         }
+
+        [TestMethod]
+        public void StockIDPropertyOK()
+        {
+            clsStock aStock = new clsStock();
+        
+            Int32 TestData = 1;
+            aStock.StockID = TestData;
+            Assert.AreEqual(aStock.StockID, TestData);
+            
+
+            
+        }
+
+        [TestMethod]
+
+        public void StockDescPropertyOK()
+        {
+            clsStock aStock = new clsStock();
+
+            string TestData = "iPhone 7 red";
+            aStock.StockDesc = TestData;
+            Assert.AreEqual(aStock.StockDesc, TestData);
+
+
+
+
+        }
+        [TestMethod]
+        public void StockNumberPropertyOK()
+        {
+            clsStock aStock = new clsStock();
+            Int32 TestData = 1;
+            aStock.StockNumber = TestData;
+            Assert.AreEqual(aStock.StockNumber, TestData);
+        }
+
+        [TestMethod]
+
+        public void StockTypePropertyOK()
+        {
+            clsStock aStock = new clsStock();
+            string TestData = "Special Offer";
+            aStock.StockType = TestData;
+            Assert.AreEqual(aStock.StockType, TestData);
+        }
+
+        [TestMethod]
+
+        public void StockStatusPropertyOK()
+        {
+            clsStock aStock = new clsStock();
+            string TestData = "Arrived";
+            aStock.StockStatus = TestData;
+            Assert.AreEqual(aStock.StockStatus, TestData);
+        }
+
+        [TestMethod]
+
+        public void FindMethodOK()
+        {
+            clsStock aStock = new clsStock();
+            Boolean Found = false;
+            Int32 StockID = 1;
+            Found = aStock.Find(StockID);
+            Assert.IsTrue(Found);
+
+        }
+
+        [TestMethod]
+        public void TestStockIDFound()
+        {
+            clsStock aStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 112;
+            Found = aStock.Find(StockID);
+            if (aStock.StockID != 112)
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(OK);
+
+         
+        }
+
+        [TestMethod]
+        public void TestDataAddedFound()
+        {
+            clsStock aStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 112;
+            Found = aStock.Find(StockID);
+            if (aStock.DateAdded != Convert.ToDateTime("15/02/2020"))
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(OK);
+            
+        }
+
+        [TestMethod]
+
+        public void TestStockDescFound()
+        {
+            clsStock aStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 112;
+            Found = aStock.Find(StockID);
+            if(aStock.StockDesc != "iPhone 7 red")
+            {
+                OK = false;
+            }
+
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestStockNumberFound()
+        {
+            clsStock aStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 112;
+            Found = aStock.Find(StockID);
+            if (aStock.StockNumber != 99)
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestStockStatusFound()
+        {
+            clsStock aStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 112;
+            Found = aStock.Find(StockID);
+            if (aStock.StockStatus != "Arrived")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+        }
+        [TestMethod]
+        public void TestStockTypeFound()
+        {
+            clsStock aStock = new clsStock();
+            Boolean Found = false;
+            Boolean OK = true;
+            Int32 StockID = 112;
+            Found = aStock.Find(StockID);
+            if (aStock.StockType != "Special Offer")
+            {
+                OK = false;
+            }
+            Assert.IsTrue(OK);
+
+        }
+
+
+       
+
+
     }
 }
+
--- DMUMobilesClasses1/clsStock.cs
+++ DMUMobilesClasses1/clsStock.cs
@@ -1,6 +1,103 @@
-﻿namespace DMUMobilesClasses1
+﻿using System;
+
+namespace DMUMobilesClasses1
 {
     public class clsStock
     {
+        //private
+        private Int32 mStockID;
+        private DateTime mDateAdded;
+        private string mStockDesc;
+        private Int32 mStockNumber;
+        private string mStockStatus;
+        private string mStockType;
+
+
+
+
+        //public
+ 
+        public DateTime DateAdded
+        {
+            get
+            {
+                return mDateAdded;
+            }
+
+            set
+            {
+                mDateAdded = value;
+            }
+        }
+        public Int32 StockID
+        {
+            get
+            {
+                return mStockID;
+            }
+
+            set
+            {
+                mStockID = value;
+            }
+
+        }
+            
+        public string StockDesc
+        {
+            get
+            {
+                return mStockDesc;
+            }
+            set
+            {
+                mStockDesc = value;
+            }
+        }
+        public Int32 StockNumber
+        {
+            get
+            {
+                return mStockNumber;
+            }
+            set
+            {
+                mStockNumber = value;
+            }
+        }
+        public string StockType
+        {
+            get
+            {
+                return mStockType;
+            }
+            set
+            {
+                mStockType = value;
+            }
+        }
+        public string StockStatus
+        {
+            get
+            {
+                return mStockStatus;
+            }
+
+            set
+            {
+                mStockStatus = value;
+            }
+        }
+
+        public bool Find(int StockID)
+        {
+            mStockType = "Special Offer";
+            mStockStatus = "Arrived";
+            mStockNumber = 99;
+            mStockDesc = "iPhone 7 red";
+            mDateAdded = Convert.ToDateTime("15/02/2020");
+            mStockID = 112;
+            return true;
+        }
     }
 }
