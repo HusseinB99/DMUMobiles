352ac9a1e42b9e755fb7ae215a99138528feebf0
Anees Hussain <P2421747@my365.dmu.ac.uk>
2020-03-13 15:41:00 +0000

    Added testing for class CustomerService.

---
+++ DMU Mobile Testing/tstCustomerService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using DMUMobilesClasses1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DMU_Mobile_Testing
+{
+    [TestClass]
+    public class tstCustomerService
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsCustomerService AnCustomerService = new clsCustomerService();
+
+            Assert.IsNotNull(AnCustomerService);
+        }
+    }
+}
